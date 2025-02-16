b06d5a5261c4ca816ec4a5c3a780ca927a0f69e0
Edson Zucchi <edsonluiz.zuchi@gmail.com>
2025-02-16 15:31:47 -0300

    interface extend remote and hotelserver

--- src/main/java/io/github/edsonzuchi/core/RoomManager.java
+++ src/main/java/io/github/edsonzuchi/core/RoomManager.java
@@ -1,9 +1,10 @@
 package io.github.edsonzuchi.core;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface RoomManager {
+public interface RoomManager extends Remote {
 
     List<String> listRooms() throws RemoteException;
     boolean bookRoom(int type, String nameGuest) throws RemoteException;
